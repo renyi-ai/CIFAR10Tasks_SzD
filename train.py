574160dd6185eeccb536822349d39f4620a2fe28
Peter Kőrösi-Szabó <koszpe@gmail.com>
2022-09-12 14:22:43 +0200

    initial commit

---
+++ train.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from data import create_dataloaders
+from model import Net
+
+
+def test(net, loader):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in loader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
+
+def train():
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    trainloader, testloader = create_dataloaders()
+    net = Net()
+    net.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(10):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+        test(net, testloader)
+
+    print('Finished Training')
+
+
+if __name__ == '__main__':
+    train()
